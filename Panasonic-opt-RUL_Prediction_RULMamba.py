edf31d0a85fd3c4799cf59776297bdf691f51a57
keepawakeyi <141097669+keepawakeyi@users.noreply.github.com>
2025-09-19 23:56:13 +0800

    Update Panasonic-opt-RUL_Prediction_RULMamba.py

--- Panasonic-opt-RUL_Prediction_RULMamba.py
+++ Panasonic-opt-RUL_Prediction_RULMamba.py
@@ -510,7 +510,7 @@ def main():
     args = parser.parse_args()
     
     # 加载电池数据
-    battery_data = np.load('E:\DATA-CODE\PatchFormer-main\datasets\Panasonic\Panasonic_Data.npy', 
+    battery_data = np.load('.\Panasonic\Panasonic_Data.npy', 
                           allow_pickle=True).item()
     
     # 创建优化器
